d73734349a526694c11b20a8ad178e9719dc7952
xuruiray <971661949@qq.com>
2018-05-17 10:03:45 +0000

    add test_data

--- gateway.py
+++ gateway.py
@@ -10,7 +10,7 @@ import json
 #参数
 max_retry_time  = 8
 irq_wait_timeout = 0.1
-scan_interval = 0.05
+scan_interval = 0.15
 
 #设备相关
 pipes = [[0x34, 0x43, 0x10, 0x10, 0x01],[0x24,0x24,0x14,0x15,0x02], [0x13,0x25,0x35,0x46,0x04]]
@@ -30,6 +30,7 @@ radio.write_register(NRF24.CONFIG,0x0c)
 
 #API相关
 host = 'http://58.87.89.234'
+#host = 'http://192.168.1.8:8080'
 upload_url = host + '/sensor/saveData'
 downloadaddr_url = host + '/sensor/loadAllAddr'
 
@@ -39,6 +40,9 @@ retry_time = 5
 
 #采集数据
 json_data = ""
+full_sign = 20
+test_data = [full_sign,full_sign,full_sign]
+test_data_str = ""
 
 class SensorAddrTemp(object):
     sensor_name = 'sn'
@@ -55,10 +59,12 @@ try:
     for addr in addrs:
         s = SensorAddrTemp(addr['sensor_name'],addr['sensor_addr'])
         addrList.append(s)
-    except requests.exceptions.ConnectTimeout:        
-            print "timeout"
-    except requests.exceptions.ReadTimeout:
-            print "timeout"
+except requests.exceptions.ConnectTimeout:        
+    print "timeout"
+except requests.exceptions.ReadTimeout:
+    print "timeout"
+except requests.exceptions.ConnectionError:
+    print "timeout"
                 
 for v in addrList:
     print v.sensor_addr
@@ -83,6 +89,9 @@ while True:
         continue
 
     if t_e-t_s > irq_wait_timeout:
+        test_data[device_num] -= 1
+        if test_data[device_num] < 0:
+            test_data[device_num] = 0
         retry_time += 1
         if retry_time < max_retry_time:
             continue
@@ -91,10 +100,15 @@ while True:
             if device_num == 3:
                 device_num = 0            
             continue
+    else:
+        test_data[device_num] += 1
+        if test_data[device_num] > full_sign:
+            test_data[device_num] = full_sign
+        
 
     recv_buffer = []
     radio.read(recv_buffer)
-
+ 
     # string op
     for i in range(len(recv_buffer)):
         recv_buffer[i] = chr(recv_buffer[i])
@@ -107,15 +121,21 @@ while True:
         
     device_num += 1
     if device_num == max_device_num:
+        test_data_str =' {"sensorName":"NRF24L01","dataMap":{"' + addrList[0].sensor_name + '":%d,"'+addrList[1].sensor_name+'":%d,"'+addrList[2].sensor_name+'":%d}},'
+        test_data_str = test_data_str%(test_data[0],test_data[1],test_data[2])
+        json_data +=  test_data_str
         json_data = '[' + string.strip(json_data,',') + ']'
-        print json_data
-       
-         
+        print json_data 
+        print time.time()
+        print test_data
+
         try:
             resp = requests.post(upload_url, json_data,timeout=10)
         except requests.exceptions.ConnectTimeout:        
             print "timeout"
         except requests.exceptions.ReadTimeout:
+            print "timeout"
+        except requests.exceptions.ConnectionError:
             print "timeout"
     
         json_data = ""
